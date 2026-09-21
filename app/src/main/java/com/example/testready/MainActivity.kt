9caa2c598640f8078ccc1061ccd091ca237378ed
Ege Kalenderoglu <egekalenderoglu@outlook.com>
2026-09-21 14:12:41 -0400

    Add environment form

--- app/src/main/java/com/example/testready/MainActivity.kt
+++ app/src/main/java/com/example/testready/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,7 +15,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,26 +32,71 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier){
+fun MainScreen(modifier: Modifier = Modifier) {
+
+    val showAddForm = remember { mutableStateOf(false) }
+    val environmentName = remember { mutableStateOf("") }
+    val baseUrl = remember { mutableStateOf("") }
+
     Column(
         modifier = modifier.padding(20.dp)
     ) {
-        Text("TestReady")
-        Spacer(modifier = Modifier.height(20.dp))
-        Text("Test Environments")
-        Spacer(modifier = Modifier.height(20.dp))
-        Text("SauceDemo QA")
-        Text("https://www.saucedemo.com")
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(onClick = { }) {
+        if (showAddForm.value == false) {
+            Text("TestReady")
+            Spacer(modifier = Modifier.height(20.dp))
+            Text("Test Environments")
+            Spacer(modifier = Modifier.height(20.dp))
+            Text("SauceDemo QA")
+            Text("https://www.saucedemo.com")
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                onClick = {
+                    showAddForm.value = true
+                }) {
+                Text("Add Environment")
+            }
+        } else {
             Text("Add Environment")
+            Spacer(modifier = Modifier.height(20.dp))
+            Text("Environment Name")
+
+            OutlinedTextField(
+                value = environmentName.value,
+                onValueChange = {
+                    environmentName.value = it
+                }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text("Base URL")
+
+            OutlinedTextField(
+                value = baseUrl.value,
+                onValueChange = {
+                    baseUrl.value = it
+                }
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = { }
+            ) {
+                Text("Save")
+            }
+            Button(
+                onClick = {
+                    showAddForm.value = false
+                }
+            ) {
+                Text("Cancel")
+
+            }
+        }
         }
     }
-}
-@Preview(showBackground = true)
-@Composable
-fun TestReadyPreview() {
-    TestReadyTheme {
-        MainScreen()
+    @Preview(showBackground = true)
+    @Composable
+    fun TestReadyPreview() {
+        TestReadyTheme {
+            MainScreen()
+        }
     }
-}
